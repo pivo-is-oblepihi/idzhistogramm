b199f2c9a0b987a36ae2ebac37d553c61879f039
Polina <prrshka@mail.ru>
2024-04-28 02:55:00 +0300

    init

---
+++ main.cpp
@@ -0,0 +1,44 @@
+#include <algorithm>
+#include <iostream>
+#include <vector>
+#include <ctime>
+
+using namespace std;
+
+vector <int> mas(int n) {
+    vector <int> a(n);
+    srand(time(0));
+    for (int i = 0; i < n; i++)
+        a[i] = rand()%100;
+    return a;
+}//создаём массив в динамической памяти и заполняем его рандомными значениями от 0 до 100
+
+void gistogram(int k) {
+    vector <int> data = mas(k);//количество элементов в массиве
+
+    int max_value = *max_element(data.begin(), data.end());//находит максимальный элемент в массиве
+
+    for (int i = max_value; i >= 0; --i) {
+        cout.width(2);
+        cout << i << " | ";
+
+        for (int j = 0; j < data.size(); ++j) {
+            if (data[j] == i) {
+                cout << "*";
+            }
+            else {
+                cout << "";
+            }
+        }//алгоритм построения гистограммы
+        cout << endl;
+    }
+    cout << "---------------------------------------" << endl;
+    cout << "  ";
+
+    return;
+}
+
+int main()
+{
+    gistogram(1000);
+}
