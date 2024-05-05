f1e500e504ef15aaf016338b688c58cffba2102a
Polina <prrshka@mail.ru>
2024-05-06 00:10:27 +0300

    я что-то хочу, но не понимаю что

--- main.cpp
+++ main.cpp
@@ -3,7 +3,38 @@
 #include <vector>
 #include <ctime>
 
+
+
+
+#include <QApplication>
+#include <QMainWindow>
+#include <QLabel>
+
 using namespace std;
+
+int exe(QApplication& app) {
+    return app.exec();
+}
+
+int main(int arge, char **argv){
+    cerr << "create app \n ";
+    QApplication app(arge, argv);
+
+    int result = exe(app);
+    cerr << " at qpp redult: " << result << endl;
+    return result;
+
+}
+
+
+
+
+
+
+
+
+
+
 
 //создаём массив в динамической памяти и заполняем его рандомными значениями от 0 до 100
 vector <int> mas(int n) {
